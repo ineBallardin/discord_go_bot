4cd79d6ad446e7ac3ce9d3946a27f36d35abe5f8
Ine Ballardin <ineballardin@gmail.com>
2023-12-14 18:44:53 -0300

    create a bot.go file

---
+++ src/bot/bot.go
@@ -0,0 +1,18 @@
+package main
+
+type Command struct {
+	Data    string
+	Execute func()
+}
+
+type Bot struct {
+	Cooldowns map[string]string
+	Commands  map[string]Command
+}
+
+func createBot() *Bot {
+	return &Bot{
+		Cooldowns: make(map[string]string),
+		Commands:  make(map[string]Command),
+	}
+}
