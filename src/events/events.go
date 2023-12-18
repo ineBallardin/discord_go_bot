575aa75c9285816eedb4723da039ab3ccd1f5e14
Ine Ballardin <ineballardin@gmail.com>
2023-12-18 13:51:08 -0300

    add voice channel counter

--- src/events/events.go
+++ src/events/events.go
@@ -6,5 +6,6 @@ func RegisterEvents(s *discordgo.Session) {
 	s.AddHandler(PingMessageCreate)
 	s.AddHandler(MessageCounter)
 	s.AddHandler(ReactionCounter)
+	s.AddHandler(VoiceChannelCounter)
 	// Adicionar mais manipuladores de eventos aqui cfe necessário
 }
---
+++ src/events/voiceChannelCounter.go
@@ -0,0 +1,65 @@
+package events
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type VoiceChannelStats struct {
+	UserID    string
+	Roles     []string
+	ChannelID string
+	JoinTime  time.Time
+	LeaveTime time.Time
+	TotalTime time.Duration
+}
+
+var voiceChannelStats = make(map[string]*VoiceChannelStats)
+
+func VoiceChannelCounter(s *discordgo.Session, v *discordgo.VoiceStateUpdate) {
+	stats, ok := voiceChannelStats[v.UserID]
+	if !ok {
+		stats = &VoiceChannelStats{
+			UserID:    v.UserID,
+			ChannelID: v.ChannelID,
+			JoinTime:  time.Now(),
+		}
+		voiceChannelStats[v.UserID] = stats
+	} else {
+		stats.LeaveTime = time.Now()
+		stats.TotalTime = stats.LeaveTime.Sub(stats.JoinTime)
+		if stats.TotalTime.Minutes() >= 5 {
+			member, err := s.GuildMember(v.GuildID, v.UserID)
+			if err != nil {
+				log.Println("Erro ao obter membro do servidor:", err)
+				return
+			}
+			stats.Roles = make([]string, len(member.Roles))
+			for i, roleID := range member.Roles {
+				role, err := s.State.Role(v.GuildID, roleID)
+				if err != nil {
+					log.Println("Erro ao obter cargo:", err)
+				}
+				stats.Roles[i] = role.Name
+			}
+
+			isImpulserPro := false
+			for _, role := range stats.Roles {
+				if role == "impulserPRO" {
+					isImpulserPro = true
+					break
+				}
+			}
+
+			_, err = s.ChannelMessageSend("1101510837555974176", fmt.Sprintf("## Contador de Canais de Voz\n**Membro:** <@%s>,\n- **impulserPRO:** %v\n- Canal: <#%s>\n- Tempo total: %v minutos", stats.UserID, isImpulserPro, stats.ChannelID, stats.TotalTime.Minutes()))
+			if err != nil {
+				log.Println("Erro ao enviar mensagem para o canal:", err)
+			}
+		}
+		stats.JoinTime = time.Now()
+		stats.ChannelID = v.ChannelID
+	}
+}
