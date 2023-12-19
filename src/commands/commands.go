30b1a3e13d08030a49cd8c85f16de84afb18820a
Ine Ballardin <ineballardin@gmail.com>
2023-12-19 17:44:58 -0300

    fix: Correct command registration in commands.go

--- src/commands/commands.go
+++ src/commands/commands.go
@@ -10,6 +10,7 @@ type SlashCommand struct {
 	Name        string
 	Description string
 	Handler     func(s *discordgo.Session, i *discordgo.InteractionCreate)
+	Options     []*discordgo.ApplicationCommandOption
 }
 
 var SlashCommands []SlashCommand
@@ -19,11 +20,23 @@ func RegisterSlashCommands(s *discordgo.Session, guildID string) {
 		_, err := s.ApplicationCommandCreate(s.State.User.ID, guildID, &discordgo.ApplicationCommand{
 			Name:        command.Name,
 			Description: command.Description,
+			Options:     command.Options,
 		})
 		if err != nil {
 			log.Printf("Cannot create '%v' command: %v", command.Name, err)
+		} else {
+			log.Printf("Command '%v' registered successfully", command.Name)
 		}
-
-		s.AddHandler(command.Handler)
 	}
+
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if i.Type == discordgo.InteractionApplicationCommand {
+			for _, command := range SlashCommands {
+				if i.ApplicationCommandData().Name == command.Name {
+					command.Handler(s, i)
+					break
+				}
+			}
+		}
+	})
 }
